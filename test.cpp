ddf14444fd7f897b51933f65daf40c4699c8dd89
Alex393 <duknieve@gmail.com>
2024-11-23 13:53:38 -0800

    Changes to main
    Co-authored-by: AlexanderHartley <AlexanderHartley@users.noreply.github.com>

--- test.cpp
+++ test.cpp
@@ -5,5 +5,17 @@ int main()
 {
     cout << "yikes"<< endl;
     cout << "I am testing out branches";
-    cout << "testing out live share"
+    cout << "testing out live share";
+    cout << "Testing Testing"; 
+    int x = 10;
+    for (int n = 0; n < x; n++ )
+    {
+        cout << "Hey a loop and a change";
+    }
+    while (x > 0)
+    {
+        cout << "Testing ";
+        --x;
+    } 
+
 } 
